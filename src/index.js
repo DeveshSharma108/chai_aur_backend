f8a0d9207270faadf20a367782aa6940cd820dab
DeveshSharma108 <137079248+DeveshSharma108@users.noreply.github.com>
2024-07-11 10:50:46 +0530

    add files to utils

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@
 
 import dotenv from "dotenv"
 import connecDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config(
     {path: './env'}
 )
 
-connecDB()
+connecDB().
+then(()=>{
+    try {
+        app.listen(process.env.PORT || 8000,()=>{
+            console.log("App is running on port ",process.env.PORT || 8000)
+        })
+    } catch (error) {
+        console.log("Error related to app",error)
+        throw error
+    }
+}).
+catch((error)=>{
+    console.log("Error while connecting to the MongoDB database")
+    console.log(error)
+})
 
 
 
