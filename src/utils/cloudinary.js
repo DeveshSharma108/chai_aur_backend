d31ff7b570bb10ef9ed180ca1c8b11d53ff7651d
DeveshSharma108 <137079248+DeveshSharma108@users.noreply.github.com>
2024-07-12 18:30:26 +0530

    add file upload functionality (multer and cloudinary)

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        
+        if (!localFilePath) {
+            return null
+        }
+
+        // upload file on cloudinary 
+        const uploadResult = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: 'auto'
+        })
+
+        console.log("File uploaded successfuly .....");
+        console.log(uploadResult);
+        console.log(uploadResult.url);
+        return uploadResult;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
